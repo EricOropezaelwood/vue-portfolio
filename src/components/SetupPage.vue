a871781c426bf57f4ed0024bb785e1bb29f5d30d
Eric Oropezaelwood <eric.oropezaelwood@gmail.com>
2023-05-22 14:51:56 -0500

    added white bg to arch diagram, changed setup font to dark slate

--- src/components/SetupPage.vue
+++ src/components/SetupPage.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-      <h1>Website Setup</h1>
+    <div class="setup-page">
+      <h1>How this site is running currently</h1>
+      <img src="../assets/vue-portfolio-architecture-diagram-white.png" alt="Website Architecture">
     </div>
   </template>
   
@@ -11,10 +12,29 @@
   </script>
   
   <style scoped>
-  h1 {
-    font-size: 24px;
-    margin-bottom: 10px;
-  }
-  
+.setup-page {
+  color:darkslategray;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 60vh;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.image-container {
+  margin-top: auto; /* Pushes the image to the bottom */
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
   </style>
   
