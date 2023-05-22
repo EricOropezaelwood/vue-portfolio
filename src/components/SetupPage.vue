58c1d505f8f2d0aa2e21ba8291c34534c59321dd
Eric Oropezaelwood <eric.oropezaelwood@gmail.com>
2023-05-22 13:11:29 -0500

    fixed double text by adding App.vue back in

--- src/components/SetupPage.vue
+++ src/components/SetupPage.vue
@@ -1,48 +1,6 @@
 <template>
     <div>
       <h1>Website Setup</h1>
-      <p>
-        Welcome to our website! This page provides information about how the website is set up and organized.
-      </p>
-      <h2>Repository Structure</h2>
-      <p>
-        The repository follows the following structure:
-      </p>
-      <pre>
-        .
-        ├── README.md
-        ├── babel.config.js
-        ├── jsconfig.json
-        ├── package-lock.json
-        ├── package.json
-        ├── public
-        │   ├── coffee-shop-minimal-anime.png
-        │   ├── drill-wide.jpeg
-        │   ├── favicon.ico
-        │   └── index.html
-        ├── src
-        │   ├── assets
-        │   │   ├── city-wallpaper.jpg
-        │   │   ├── fuji-minimal.png
-        │   │   ├── logo.png
-        │   │   └── vue-portfolio-architecture-diagram.png
-        │   ├── components
-        │   │   ├── AppNavbar.vue
-        │   │   ├── HomePage.vue
-        │   │   └── ImagePage.vue
-        │   ├── main.js
-        │   └── router
-        │       └── index.js
-        └── vue.config.js
-      </pre>
-      <h2>Tech Stack</h2>
-      <ul>
-        <li>Vue.js: A progressive JavaScript framework for building user interfaces.</li>
-        <li>Vue Router: The official router for Vue.js, enabling navigation between different pages or components.</li>
-        <li>Babel: A JavaScript compiler that converts modern JavaScript code into backward-compatible versions.</li>
-        <li>Webpack: A module bundler used to bundle JavaScript files and other assets.</li>
-        <li>npm: The package manager for JavaScript that handles dependencies and project management.</li>
-      </ul>
     </div>
   </template>
   
@@ -58,32 +16,5 @@
     margin-bottom: 10px;
   }
   
-  h2 {
-    font-size: 20px;
-    margin-top: 20px;
-    margin-bottom: 10px;
-  }
-  
-  p {
-    margin-bottom: 15px;
-  }
-  
-  pre {
-    font-family: monospace;
-    background-color: #f7f7f7;
-    padding: 10px;
-    overflow-x: auto;
-  }
-  
-  ul {
-    margin-top: 10px;
-    margin-bottom: 15px;
-    list-style-type: disc;
-    padding-left: 20px;
-  }
-  
-  li {
-    margin-bottom: 5px;
-  }
   </style>
   
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './components/HomePage.vue'
+import App from './App.vue' // Update the import to use App.vue
 import router from './router'
 
 createApp(App).use(router).mount('#app')
