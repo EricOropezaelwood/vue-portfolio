aa5ebfb2bd5842b7dfdf40f7d730d37d572b3bea
Eric Oropezaelwood <10686338+EricOropezaelwood@users.noreply.github.com>
2023-05-30 15:17:47 -0500

    Update index.js history
    changed from createWebHistory -> createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '@/components/HomePage.vue'
 import SetupPage from '@/components/SetupPage.vue';
-
 
 const routes = [
   {
@@ -13,12 +12,11 @@ const routes = [
     name: 'SetupPage',
     component: SetupPage
   }
-  
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes, // short for `routes: routes`
+  history: createWebHashHistory(),
+  routes,
 })
 
 export default router
